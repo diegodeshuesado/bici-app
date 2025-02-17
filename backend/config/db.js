812ff892ee04439c5bb5562cd09dc3d64384ada7
diegodeshuesado <diegogut93@gmail.com>
2025-02-17 14:49:03 -0600

    Protección de credenciales con .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config(); // Cargar variables de entorno
 
 const pool = new Pool({
-  user: 'app_user',       // Usuario que creaste en PostgreSQL
-  host: 'localhost',      // Servidor local
-  database: 'bicicletas_db', // Nombre de la base de datos
-  password: 'admin', // Contraseña del usuario
-  port: 5432, 
+  user: process.env.DB_USER,       // Usuario desde .env
+  host: process.env.DB_HOST,      // Host desde .env
+  database: process.env.DB_NAME,  // Nombre de la base de datos desde .env
+  password: process.env.DB_PASSWORD, // Contraseña desde .env
+  port: process.env.DB_PORT,      // Puerto desde .env
 });
 
 module.exports = pool;
